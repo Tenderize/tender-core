30b20920ee406c6de15ef5d1df542e526fdc4436
Nico Vergauwen <vergauwennico@gmail.com>
2021-05-06 18:24:14 +0200

    livepeer deployment script

--- deploy/Livepeer.ts
+++ deploy/Livepeer.ts
@@ -5,18 +5,25 @@ import {
 } from "hardhat";
 
 import {
-  TenderToken, Tenderizer, ElasticSupplyPool
+  TenderToken, Tenderizer, ElasticSupplyPool, ERC20, Controller
 } from "../typechain/";
+import { BigNumber } from '@ethersproject/bignumber';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) { // the deploy function receive the hardhat runtime env as argument
   const {deployments, getNamedAccounts} = hre // Get the deployments and getNamedAccounts which are provided by hardhat-deploy
   const {deploy} = deployments // the deployments field itself contains the deploy function
+
+  let bpoolAddr: string
 
   const {deployer} = await getNamedAccounts() // Fetch named accounts from hardhat.config.ts
 
   const safeMathFixture = await deployments.fixture(["SafeMath"])
 
   const balancerFixture = await deployments.fixture(["Balancer"])
+
+  const steakAmount = process.env.STEAK_AMOUNT || "0"
+
+  const bootstrapSupply: BigNumber = ethers.utils.parseEther(steakAmount).div(2)
 
   const tenderizer = await deploy('Livepeer', {
     from: deployer,
@@ -46,13 +53,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) { /
   }
   
   const poolParams = {
-    "poolTokenSymbol": "BAL-REBASING-SMART-V1-AMPL-USDC",
-    "poolTokenName": "Balancer Rebasing Smart Pool Token V1 (AMPL-USDC)",
+    "poolTokenSymbol": "BAL-REBASING-SMART-V1-tLPT-LPT",
+    "poolTokenName": "Balancer Rebasing Smart Pool Token V1 (tLPT-LPT)",
     "constituentTokens": [
       tenderToken.address,
       process.env.LIVEPEER_TOKEN
     ],
-    "tokenBalances": ["100000000000","100000000"],
+    "tokenBalances": [bootstrapSupply, bootstrapSupply],
     "tokenWeights": ["7071067811870000000", "7071067811870000000"],
     "swapFee": "3000000000000000"
   }
@@ -70,16 +77,32 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) { /
 
   const controller = await deploy('Controller', {
     from: deployer,
-    args: [ethers.constants.AddressZero, tenderizer.address, tenderToken.address, ethers.constants.AddressZero]
+    args: [process.env.LIVEPEER_TOKEN, tenderizer.address, tenderToken.address, esp.address]
   })
 
   const TenderToken: TenderToken = (await ethers.getContractAt('TenderToken', tenderToken.address)) as TenderToken
   const Tenderizer: Tenderizer = (await ethers.getContractAt('Tenderizer', tenderizer.address)) as Tenderizer
   const Esp: ElasticSupplyPool = (await ethers.getContractAt('ElasticSupplyPool', esp.address)) as ElasticSupplyPool
-
+  const Steak: ERC20 = (await ethers.getContractAt('ERC20', process.env.LIVEPEER_TOKEN || ethers.constants.AddressZero)) as ERC20
+  const Controller: Controller = (await ethers.getContractAt('Controller', controller.address)) as Controller
   await TenderToken.transferOwnership(controller.address, {from: deployer})
   await Tenderizer.transferOwnership(controller.address, {from: deployer})
   await Esp.setController(controller.address) 
+
+  const pcTokenSupply = '1000000000000000000000' // 1000e18
+  const minimumWeightChangeBlockPeriod = 10;
+  const addTokenTimeLockInBlocks = 10;
+
+  await Steak.approve(controller.address, bootstrapSupply)
+  await Controller.deposit(bootstrapSupply)
+
+  await Steak.approve(esp.address, bootstrapSupply)
+  await TenderToken.approve(esp.address, bootstrapSupply)
+
+  await Esp.createPool(pcTokenSupply, minimumWeightChangeBlockPeriod, addTokenTimeLockInBlocks)
+  bpoolAddr = await Esp.bPool()
+
+  await Esp.setController(controller.address)
 
   // TODO: get steak and tenderToken
   // create ESP 
