38b1264dbd67bfd0620afc08bf4ac8dcd6fcb38c
Reuben Rodrigues <reuben.r@smarter.codes>
2021-06-10 06:16:03 -0700

    Matic: unstaking/withdrawal

--- contracts/tenderizer/integrations/matic/Matic.sol
+++ contracts/tenderizer/integrations/matic/Matic.sol
@@ -63,50 +63,58 @@ contract Matic is Tenderizer {
         steak.approve(maticStakeManager, amount);
 
         // stake tokens
-        uint256 fxRate = matic.exchangeRate();
-        if (fxRate == 0) fxRate = 1;
-        uint256 min = MathUtils.percOf(amount, EXCHANGE_RATE_PRECISION, fxRate);
-        matic_.buyVoucher(amount, min);
+        matic_.buyVoucher(amount, tokensToShares(amount));
     }
 
     function _unstake(address _account, address _node, uint256 _amount) internal override {
-        //  // Check that no withdrawal is pending
-        // require(pendingWithdrawals[_account] == 0, "PENDING_WITHDRAWAL");
-        // uint256 amount = _amount;
+        // use default validator share contract if _node isn't specified
+        IMatic matic_ = matic;
+        if (_node != address(0)) {
+            matic_ = IMatic(_node);
+        }
 
-        // // Sanity check. Controller already checks user deposits and withdrawals > 0
-        // if (_account != owner()) require(amount > 0, "ZERO_AMOUNT");
-        // if (amount == 0) {
-        //     amount = IERC20(steak).balanceOf(address(this));
-        // }
+        // subtract principle
+        currentPrincipal = currentPrincipal - _amount;
 
-        // // if no _node is specified, stake towards the default node
-        // IMatic node_ = IMatic(_node);
-        // if (_node == address(0)) {
-        //     node_ = IMatic(node);
-        // }
-
-        // currentPrincipal = currentPrincipal - _amount;
-
-      
-        // // undelegate shares
-        // node_.sellVoucher(0);
+        // Track unstake for withdrawal
+        pendingWithdrawals[_account] += _amount;
+        totalPendingWithdrawals += _amount;
     }
     
     function _unstakeFromProtocol() internal override {
-        
+        // Check that are pending withrawals
+        require(totalPendingWithdrawals > 0, "NO_TOTAL_PENDING_WITHDRAWAL");
+       
+        // unstake
+        matic.sellVoucher(tokensToShares(totalPendingWithdrawals));
     }
 
     function _withdraw(address _account, uint256 /*_amount*/) internal override {
-        // // Check that a withdrawal is pending
-        // uint256 amount = graph.withdrawDelegated(node, ZERO_ADDRESS);
+        // Get users pending withdrawal
+        uint256 amount = pendingWithdrawals[_account];
+        
+        // Check that some amount is unstaked
+        require(amount > 0, "NO_PENDING_WITHDRAWAL");
 
-        // // Transfer amount from unbondingLock to _account
-        // steak.transfer(_account, amount);
+        // Check that no withdrawFromProtocol() pending
+        require(totalPendingWithdrawals == 0, "GOVERNANANCE_UNSTAKE_PENDING");
+
+        // Delete pending withrawal for user
+        delete pendingWithdrawals[_account];
+
+        // Transer steak to _account
+        steak.transfer(_account, amount);
     }
 
     function _withdrawFromProtocol() internal override {
-        
+        // Check that are pending withrawals
+        require(totalPendingWithdrawals > 0, "NO_TOTAL_PENDING_WITHDRAWAL");
+
+        // Set 0 pending withdrawals
+        totalPendingWithdrawals = 0;
+
+        // Claim the unstaked tokens
+        matic.unstakeClaimTokens();
     }
 
     function _claimRewards() internal override {
@@ -117,7 +125,6 @@ contract Matic is Tenderizer {
         } catch {
 
         } 
-
 
         // calculate rewards and fees
         uint256 rewards;
@@ -163,4 +170,9 @@ contract Matic is Tenderizer {
         maticStakeManager = _stakingContract;
     }
 
+    function tokensToShares(uint256 _tokens) internal view returns (uint256) {
+        uint256 fxRate = matic.exchangeRate();
+        if (fxRate == 0) fxRate = 1;
+        return MathUtils.percOf(_tokens, EXCHANGE_RATE_PRECISION, fxRate);
+    }
 }
