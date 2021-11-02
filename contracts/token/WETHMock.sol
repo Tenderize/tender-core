2d81b6037e7028dfbe28cf7dea49244cfae8ea08
Reuben Rodrigues <63185771+reubenr0d@users.noreply.github.com>
2021-11-02 15:04:43 +0530

    Livepeer: 1Inch to Uniswap (#113)

---
+++ contracts/token/WETHMock.sol
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2021 Tenderize <info@tenderize.me>
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.4;
+
+contract WETHMock {
+     function deposit() external payable {}
+      function approve(address guy, uint wad) public returns (bool) {}
+}
