fadbbdb53664cf413c463d21479ea16b06ce0193
Nico Vergauwen <vergauwennico@gmail.com>
2021-11-17 02:51:12 +0100

    TenderSwap: add/remove liquidity

--- contracts/liquidity/SwapUtils.sol
+++ contracts/liquidity/SwapUtils.sol
@@ -4,6 +4,7 @@
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "../libs/MathUtils.sol";
+import "./LiquidityPoolToken.sol";
 
 pragma solidity 0.8.4;
 
@@ -46,13 +47,37 @@ library SwapUtils {
         uint256 precisionMultiplier;
     }
 
+    // Struct storing variables used in calculations in the
+    // {add,remove}Liquidity functions to avoid stack too deep errors
+    struct ManageLiquidityInfo {
+        uint256 d0;
+        uint256 d1;
+        uint256 d2;
+        uint256 preciseA;
+        LiquidityPoolToken lpToken;
+        uint256 totalSupply;
+        PooledToken[2] tokens;
+        uint256[2] oldBalances;
+        uint256[2] newBalances;
+    }
+
+    // Struct storing variables used in calculations in the
+    // calculateWithdrawOneTokenDY function to avoid stack too deep errors
+    struct CalculateWithdrawOneTokenDYInfo {
+        uint256 d0;
+        uint256 d1;
+        uint256 newY;
+        uint256 feePerToken;
+        uint256 preciseA;
+    }
+
     /**
      * @notice swap two tokens in the pool
      * @param tokenFrom the token to sell
      * @param tokenTo the token to buy
      * @param dx the number of tokens to sell. If the token charges a fee on transfers,
      * use the amount that gets transferred after the fee.
-     * @param amplifactionParams amplification parameters for the pool
+     * @param amplificationParams amplification parameters for the pool
      * @param feeParams fee parameters for the pool
      * @param dx the amount of tokens the user wants to sell
      * @param minDy the min amount the user would like to receive, or revert.
@@ -63,7 +88,7 @@ library SwapUtils {
         PooledToken storage tokenTo,
         uint256 dx,
         uint256 minDy,
-        Amplification storage amplifactionParams,
+        Amplification storage amplificationParams,
         FeeParams storage feeParams
     ) external returns (uint256) {
         // transfer tokens
@@ -75,7 +100,7 @@ library SwapUtils {
             tokenFrom,
             tokenTo,
             dx,
-            amplifactionParams,
+            amplificationParams,
             feeParams
         );
         require(dy >= minDy, "Swap didn't result in min tokens");
@@ -94,7 +119,7 @@ library SwapUtils {
      * @param tokenTo the token to buy
      * @param dx the number of tokens to sell. If the token charges a fee on transfers,
      * use the amount that gets transferred after the fee.
-     * @param amplifactionParams amplification parameters for the pool
+     * @param amplificationParams amplification parameters for the pool
      * @param feeParams fee parameters for the pool
      * @return dy the number of tokens the user will get
      */
@@ -102,16 +127,320 @@ library SwapUtils {
         PooledToken storage tokenFrom,
         PooledToken storage tokenTo,
         uint256 dx,
-        Amplification storage amplifactionParams,
+        Amplification storage amplificationParams,
         FeeParams storage feeParams
     ) external view returns (uint256 dy) {
         (dy, ) = _calculateSwap(
             tokenFrom,
             tokenTo,
             dx,
-            amplifactionParams,
+            amplificationParams,
             feeParams
         );
+    }
+
+    /**
+     * @notice Add liquidity to the pool
+     * @param tokens Swap struct to read from and write to
+     * @param amounts the amounts of each token to add, in their native precision
+     * @param minToMint the minimum LP tokens adding this amount of liquidity
+     * should mint, otherwise revert. Handy for front-running mitigation
+     * allowed addresses. If the pool is not in the guarded launch phase, this parameter will be ignored.
+     * @param amplificationParams amplification parameters for the pool
+     * @param feeParams fee parameters for the pool
+     * @param lpToken Liquidity pool token contract
+     * @return amount of LP token user received
+     */
+    function addLiquidity(
+        PooledToken[2] memory tokens,
+        uint256[2] memory amounts,
+        uint256 minToMint,
+        Amplification storage amplificationParams,
+        FeeParams storage feeParams,
+        LiquidityPoolToken lpToken
+    ) external returns (uint256) {
+        // current state
+        ManageLiquidityInfo memory v = ManageLiquidityInfo(
+            0,
+            0,
+            0,
+            _getAPrecise(amplificationParams),
+            lpToken,
+            0,
+            tokens,
+            [uint256(0), uint256(0)],
+            [uint256(0), uint256(0)]
+        );
+        v.totalSupply = v.lpToken.totalSupply();
+
+        // Get the current pool invariant d0
+        if (v.totalSupply != 0) {
+            uint256 _bal0 = _getTokenBalance(tokens[0].token);
+            uint256 _bal1 = _getTokenBalance(tokens[1].token);
+            v.oldBalances = [_bal0, _bal1];
+            uint256 xp0 = _xp(_bal0, tokens[0].precisionMultiplier);
+            uint256 xp1 = _xp(_bal1, tokens[1].precisionMultiplier);
+            v.d0 = getD(xp0, xp1, v.preciseA);
+        }
+
+        // Transfer the tokens
+        for (uint256 i=0; i < tokens.length; i++) {
+            tokens[i].token.safeTransferFrom(msg.sender, address(this), amounts[i]);
+        }
+
+        // calculate pool invariant after balance changes d1
+        {
+            uint256 _bal0 = _getTokenBalance(tokens[0].token);
+            uint256 _bal1 = _getTokenBalance(tokens[1].token);
+            v.newBalances = [_bal0, _bal1];
+            uint256 _xp0 = _xp(_bal0, tokens[0].precisionMultiplier);
+            uint256 _xp1 = _xp(_bal1, tokens[1].precisionMultiplier);
+            v.d1 = getD(_xp0, _xp1, v.preciseA);
+            require(v.d1 > v.d0, "D1 <= D0");
+        }
+
+        // calculate swap fees
+        v.d2 = v.d1;
+
+        // first entrant doesn't pay fees
+        if (v.totalSupply != 0) {
+            uint256 feePerToken = _feePerToken(feeParams.swapFee);
+            uint256[] memory fees;
+
+            for (uint256 i=0; i < tokens.length; i++) {
+                uint256 idealBal = v.d1 * v.oldBalances[i] / v.d0;
+                feePerToken * idealBal.difference(v.newBalances[i]) / FEE_DENOMINATOR;
+                fees[i] = feePerToken * idealBal.difference(v.newBalances[i]) / FEE_DENOMINATOR;
+                v.newBalances[i] = v.newBalances[i] - fees[i];
+                // TODO: handle admin fee
+            }
+
+            // calculate invariant after subtracting fees, d2
+            {
+                uint256 _xp0 = _xp(v.newBalances[0], tokens[0].precisionMultiplier);
+                uint256 _xp1 = _xp(v.newBalances[1], tokens[1].precisionMultiplier);
+                v.d2 = getD(_xp0, _xp1, v.preciseA);
+            }
+        }
+
+        uint256 toMint;
+        if (v.totalSupply == 0) {
+            toMint = v.d1;
+        } else {
+            toMint = (v.d2 - v.d0) * v.totalSupply / v.d0;
+        }
+
+        require(toMint >= minToMint, "Couldn't mint min requested");
+
+        // mint the user's LP tokens
+        v.lpToken.mint(msg.sender, toMint);
+
+        return toMint;
+    }
+
+    /**
+     * @notice Burn LP tokens to remove liquidity from the pool.
+     * @dev Liquidity can always be removed, even when the pool is paused.
+     * @param amount the amount of LP tokens to burn
+     * @param tokens Swap struct to read from and write to
+     * @param minAmounts the minimum amounts of each token in the pool
+     * acceptable for this burn. Useful as a front-running mitigation
+     * @param lpToken Liquidity pool token contract
+     * @return amounts of tokens the user received
+     */
+    function removeLiquidity(
+        uint256 amount,
+        PooledToken[2] calldata tokens,
+        uint256[2] calldata minAmounts,
+        LiquidityPoolToken lpToken
+    ) external returns (uint256[2] memory) {
+        uint256 totalSupply = lpToken.totalSupply();
+
+        uint256[2] memory amounts = _calculateRemoveLiquidity(amount, tokens, totalSupply);
+
+        lpToken.burnFrom(msg.sender, amount);
+
+        for (uint256 i = 0; i < tokens.length; i++) {
+            require(amounts[i] >= minAmounts[i], "amounts[i] < minAmounts[i]");
+            tokens[i].token.safeTransfer(msg.sender, amounts[i]);
+        }
+
+        return amounts;
+    }
+
+    /**
+     * @notice Remove liquidity from the pool all in one token.
+     * @param tokenAmount the amount of the lp tokens to burn
+     * @param tokenReceive  the token you want to receive
+     * @param tokenCounterpart the counterpart token in the pool of the token you want to receive
+     * @param minAmount the minimum amount to withdraw, otherwise revert
+     * @param amplificationParams amplification parameters for the pool
+     * @param feeParams fee parameters for the pool
+     * @param lpToken Liquidity pool token contract
+     * @return amount chosen token that user received
+     */
+    function removeLiquidityOneToken(
+        uint256 tokenAmount,
+        PooledToken storage tokenReceive,
+        PooledToken storage tokenCounterpart,
+        uint256 minAmount,
+        Amplification storage amplificationParams,
+        FeeParams storage feeParams,
+        LiquidityPoolToken lpToken
+    ) external returns (uint256) {
+        uint256 totalSupply = lpToken.totalSupply();
+        require(tokenAmount <= lpToken.balanceOf(msg.sender), ">LP.balanceOf");
+
+        (uint256 dy, /*uint256 dyFee*/ ) = _calculateWithdrawOneToken(
+            tokenAmount,
+            tokenReceive,
+            tokenCounterpart,
+            totalSupply,
+            amplificationParams,
+            feeParams
+        );
+
+        require(dy >= minAmount, "dy < minAmount");
+
+        // TODO: Handle admin fee from dyFee
+
+        // Transfer tokens
+        tokenReceive.token.safeTransfer(msg.sender, dy);
+
+        // Burn LP tokens
+        lpToken.burnFrom(msg.sender, tokenAmount);
+
+        return dy;
+    }
+
+    function _calculateWithdrawOneToken(
+        uint256 tokenAmount,
+        PooledToken storage tokenReceive,
+        PooledToken storage tokenCounterpart,
+        uint256 totalSupply,
+        Amplification storage amplificationParams,
+        FeeParams storage feeParams
+    ) internal view returns (uint256, uint256) {
+        uint256 dy;
+        uint256 newY;
+        uint256 currentY;
+
+        (dy, newY, currentY) = calculateWithdrawOneTokenDY(
+            tokenAmount,
+            tokenReceive,
+            tokenCounterpart,
+            totalSupply,
+            _getAPrecise(amplificationParams),
+            feeParams.swapFee
+        );
+
+        // dy_0 (without fees)
+        // dy, dy_0 - dy
+
+        uint256 dySwapFee = (currentY - newY) / tokenReceive.precisionMultiplier - dy;
+
+        return (dy, dySwapFee);
+    }
+
+    /**
+     * @notice Calculate the dy of withdrawing in one token
+     * @param tokenAmount the amount to withdraw in the pools precision
+     * @param tokenReceive Swap struct to read from
+     * @param tokenCounterpart which token will be withdrawn
+     * @param totalSupply total supply of the lp token
+     * @return the d and the new y after withdrawing one token
+     */
+    function calculateWithdrawOneTokenDY(
+        uint256 tokenAmount,
+        PooledToken storage tokenReceive,
+        PooledToken storage tokenCounterpart,
+        uint256 totalSupply,
+        uint256 preciseA,
+        uint256 swapFee
+    )
+        internal
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256
+        )
+    {
+        // Get the current D, then solve the stableswap invariant
+        // y_i for D - tokenAmount
+        uint256 trBal = _getTokenBalance(tokenReceive.token);
+        uint256 xpR = _xp(trBal, tokenReceive.precisionMultiplier);
+
+        uint256 tcBal = _getTokenBalance(tokenCounterpart.token);
+        uint256 xpC = _xp(tcBal, tokenCounterpart.precisionMultiplier);
+
+        CalculateWithdrawOneTokenDYInfo
+            memory v = CalculateWithdrawOneTokenDYInfo(0, 0, 0, 0, 0);
+        v.preciseA = preciseA;
+        // swap from counterpart to receive (so counterpart is from and receive is to)
+        v.d0 = getD(xpC, xpR, v.preciseA);
+        v.d1 = v.d0 - (tokenAmount * v.d0 / totalSupply);
+
+        v.newY = getYD(v.preciseA, xpR, v.d1);
+
+        v.feePerToken = _feePerToken(swapFee);
+
+        // For xpR => dxExpected = xpR * d1 / d0 - newY
+        // For xpC => dxExpected = xpC - (xpC * d1 / d0)
+        // xpReduced -= dxExpected * fee / FEE_DENOMINATOR
+        uint256 xpRReduced = xpR - (xpR * v.d1 / v.d0 - v.newY) * v.feePerToken / FEE_DENOMINATOR;
+        uint256 xpCReduced = xpC - (xpC - (xpC * v.d1 / v.d0)) * v.feePerToken / FEE_DENOMINATOR;
+
+        uint256 dy = xpRReduced - getYD(v.preciseA, xpR, v.d1);
+
+        dy = (dy - 1) / tokenReceive.precisionMultiplier;
+
+        return (dy, v.newY, xpR);
+    }
+
+    /**
+     * @notice Calculate the price of a token in the pool with given
+     * precision-adjusted balances and a particular D.
+     *
+     * @dev This is accomplished via solving the invariant iteratively.
+     * See the StableSwap paper and Curve.fi implementation for further details.
+     *
+     * x_1**2 + x1 * (sum' - (A*n**n - 1) * D / (A * n**n)) = D ** (n + 1) / (n ** (2 * n) * prod' * A)
+     * x_1**2 + b*x_1 = c
+     * x_1 = (x_1**2 + c) / (2*x_1 + b)
+     *
+     * @param a the amplification coefficient * n * (n - 1). See the StableSwap paper for details.
+     * @param xpTo a precision-adjusted balance of the token to receive
+     * @param d the stableswap invariant
+     * @return the price of the token, in the same precision as in xp
+     */
+    function getYD(
+        uint256 a,
+        uint256 xpTo,
+        uint256 d
+    ) internal pure returns (uint256) {
+        uint256 c = d * d / (xpTo * NUM_TOKENS);
+        uint256 s = xpTo;
+        uint256 nA = a * NUM_TOKENS;
+
+        c = c * d * A_PRECISION / (nA * NUM_TOKENS);
+
+        uint256 b = s + (d * A_PRECISION / nA);
+        
+        uint256 yPrev;
+        uint256 y = d;
+        for (uint256 i = 0; i < MAX_LOOP_LIMIT; i++) {
+            yPrev = y;
+            uint256 num = y * y + c;
+            uint256 denom =  y * 2 + b - d;
+            y = num / denom;
+            // y = y.mul(y).add(c).div(y.mul(2).add(b).sub(d));
+            if (y.within1(yPrev)) {
+                return y;
+            }
+        }
+        revert("Approximation did not converge");
+
     }
 
     /**
@@ -124,7 +453,7 @@ library SwapUtils {
      * @param tokenTo the token to buy
      * @param dx the number of tokens to sell. If the token charges a fee on transfers,
      * use the amount that gets transferred after the fee.
-     * @param amplifactionParams amplification parameters for the pool
+     * @param amplificationParams amplification parameters for the pool
     * @param feeParams fee parameters for the pool
      * @return dy the number of tokens the user will get
      * @return dyFee the associated fee
@@ -133,16 +462,16 @@ library SwapUtils {
         PooledToken storage tokenFrom,
         PooledToken storage tokenTo,
         uint256 dx,
-        Amplification storage amplifactionParams,
+        Amplification storage amplificationParams,
         FeeParams storage feeParams
     ) internal view returns (uint256 dy, uint256 dyFee) {
         // tokenFrom balance
-        uint256 fromBalance = _getTokenBalance(tokenFrom);
+        uint256 fromBalance = _getTokenBalance(tokenFrom.token);
         // precision adjusted balance
         uint256 fromXp = _xp(fromBalance, tokenFrom.precisionMultiplier);
 
         // tokenTo balance
-        uint256 toBalance = _getTokenBalance(tokenTo);
+        uint256 toBalance = _getTokenBalance(tokenTo.token);
         // precision adjusted balance
         uint256 toXp = _xp(toBalance, tokenTo.precisionMultiplier);
         
@@ -150,7 +479,7 @@ library SwapUtils {
         uint256 x = _xp(dx, tokenFrom.precisionMultiplier) + fromXp;
 
         uint256 y = getY(
-            _getAPrecise(amplifactionParams),
+            _getAPrecise(amplificationParams),
             fromXp,
             toXp,
             x
@@ -159,6 +488,22 @@ library SwapUtils {
         dy = toXp - y - 1;
         dyFee = dy * feeParams.swapFee / FEE_DENOMINATOR;
         dy = (dy - dyFee) / tokenTo.precisionMultiplier;
+    }
+
+    function _calculateRemoveLiquidity(
+        uint256 amount,
+        PooledToken[2] calldata tokens,
+        uint256 totalSupply
+    ) internal view returns (uint256[2] memory) {
+        require(amount <= totalSupply, "Cannot exceed total supply");
+
+        uint256[2] memory outAmounts;
+
+        for (uint256 i = 0; i < tokens.length; i++) {
+            uint256 balance = _getTokenBalance(tokens[i].token);
+            outAmounts[i] = balance * amount / totalSupply;
+        }
+        return outAmounts;
     }
 
     /**
@@ -269,6 +614,19 @@ library SwapUtils {
         uint256 precisionMultiplier
     ) internal pure returns (uint256) {
         return balance * precisionMultiplier;
+    }
+
+    /**
+     * @notice internal helper function to calculate fee per token multiplier used in
+     * swap fee calculations
+     * @param swapFee swap fee for the tokens
+     */
+    function _feePerToken(uint256 swapFee)
+        internal
+        pure
+        returns (uint256)
+    {
+        return swapFee / NUM_TOKENS;
     }
 
     // =============================================
@@ -432,10 +790,10 @@ library SwapUtils {
     // =============================================
 
     function getTokenBalance(PooledToken storage _token) external view returns (uint256) {
-        return _getTokenBalance(_token);
+        return _getTokenBalance(_token.token);
     }
 
-    function _getTokenBalance(PooledToken storage _token) internal view returns (uint256) {
-        return _token.token.balanceOf(address(this));
+    function _getTokenBalance(IERC20 _token) internal view returns (uint256) {
+        return _token.balanceOf(address(this));
     }
 }
