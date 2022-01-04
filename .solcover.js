2c1798b9a6526a854befcb5c356ccfcc083dacce
Reuben Rodrigues <reubenrodrigues24@gmail.com>
2022-01-04 21:55:32 +0530

    solcover skipFiles

--- .solcover.js
+++ .solcover.js
@@ -2,15 +2,7 @@ module.exports = {
     skipFiles: [
         'libs',
         'test',
-        'Faucet.sol',
-        'tenderizer/test',
-        'tenderizer/integrations/livepeer/LivepeerMock.sol',
-        'tenderizer/integrations/graph/GraphMock.sol',
-        'tenderizer/integrations/matic/MaticMock.sol',
-        'tenderizer/integrations/audius/AudiusMock.sol',
-        'liquidity/ElasticSupplyPool.sol',
-        'liquidity/UniswapPoolMock.sol',
-        'liquidity/UniswapRouterMock.sol',
-        'token/WETHMock.sol',
+        'interfaces',
+        'helpers/Faucet.sol'
     ]
   };
