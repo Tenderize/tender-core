b405f09f16822e26ce4564e1ce4d025ff296d5f1
Mark Vujevits <vujevits.mark@gmail.com>
2022-01-14 11:06:19 +0100

    fix add liquidity script

--- scripts/add_liquidity.ts
+++ scripts/add_liquidity.ts
@@ -4,9 +4,7 @@
 // export INFURA_KEY = ...
 // npx hardhat run --network rinkeby scripts/add_liquidity.ts
 
-import {
-  TenderToken, Tenderizer, TenderSwap, ERC20
-} from '../typechain'
+import { TenderToken, Tenderizer, TenderSwap, ERC20 } from '../typechain'
 
 import { signERC2612Permit } from 'eth-permit'
 import { utils } from 'ethers'
@@ -16,58 +14,46 @@ const hre = require('hardhat')
 async function main () {
   const tenderizer = process.env.TENDERIZER as string
   const amount = utils.parseEther(process.env.AMOUNT || '0')
-  const provider = hre.ethers.providers.provider
+  const provider = hre.ethers.provider
 
   const signer = (await hre.ethers.getSigners())[0]
 
   const Tenderizer: Tenderizer = (await hre.ethers.getContractAt('Tenderizer', tenderizer)) as Tenderizer
-  const TenderToken: TenderToken = (await hre.ethers.getContractAt('TenderToken', await Tenderizer.tenderToken()))
-  const Steak: ERC20 = (await hre.ethers.getContractAt('ERC20', await Tenderizer.steak()))
-  const TenderSwap: TenderSwap = (await hre.ethers.getContractAt('TenderSwap', await Tenderizer.tenderSwap())) as TenderSwap
+  const TenderToken: TenderToken = await hre.ethers.getContractAt('TenderToken', await Tenderizer.tenderToken())
+  const Steak: ERC20 = await hre.ethers.getContractAt('ERC20', await Tenderizer.steak())
+  const TenderSwap: TenderSwap = (await hre.ethers.getContractAt(
+    'TenderSwap',
+    await Tenderizer.tenderSwap()
+  )) as TenderSwap
 
-  const deadline = ((new Date()).getTime() + 20 * 6000) / 1000
-
-  const steakApproval = await signERC2612Permit(
-    provider,
-    Steak.address,
-    signer.address,
-    TenderSwap.address,
-    amount.toString(),
-    deadline
-  )
+  const deadline = Math.floor((new Date().getTime() + 20 * 60000) / 1000)
+  const approveTx = await Steak.approve(TenderSwap.address, amount)
+  await approveTx.wait()
 
   const tenderTokenApproval = await signERC2612Permit(
     provider,
     TenderToken.address,
     signer.address,
     TenderSwap.address,
-    amount.toString(),
-    deadline
+    amount.toString()
   )
 
   const tx = await TenderSwap.multicall([
-    TenderSwap.interface.encodeFunctionData(
-      'selfPermit',
-      [Steak.address, amount, deadline, steakApproval.v, steakApproval.r, steakApproval.s]
-    ),
-    TenderSwap.interface.encodeFunctionData(
-      'selfPermit',
-      [TenderToken.address, amount, deadline, tenderTokenApproval.v, tenderTokenApproval.r, tenderTokenApproval.s]
-    ),
-    TenderSwap.interface.encodeFunctionData(
-      'addLiquidity',
-      [[amount, amount], 0, deadline]
-    )
+    TenderSwap.interface.encodeFunctionData('selfPermit', [
+      TenderToken.address,
+      amount,
+      tenderTokenApproval.deadline,
+      tenderTokenApproval.v,
+      tenderTokenApproval.r,
+      tenderTokenApproval.s
+    ]),
+    TenderSwap.interface.encodeFunctionData('addLiquidity', [[amount, amount], 0, deadline])
   ])
 
   await tx.wait()
 
-  console.log(
-    'TenderToken reserve', utils.formatEther(await TenderSwap.getToken0Balance())
-  )
-  console.log(
-    'Steak Reserve', utils.formatEther(await TenderSwap.getToken1Balance())
-  )
+  console.log('TenderToken reserve', utils.formatEther(await TenderSwap.getToken0Balance()))
+  console.log('Steak Reserve', utils.formatEther(await TenderSwap.getToken1Balance()))
 }
 
 main()
