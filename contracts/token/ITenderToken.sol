238964772f9ceaeac0f1819b9ba3717a63582915
Nico Vergauwen <vergauwennico@gmail.com>
2022-01-26 17:11:23 +0100

    TenderToken: fix return doctags

--- contracts/token/ITenderToken.sol
+++ contracts/token/ITenderToken.sol
@@ -32,7 +32,7 @@ interface ITenderToken {
 
     /**
      * @notice The number of decimals the TenderToken uses.
-     * @return the number of decimals for getting user representation of a token amount.
+     * @return decimals the number of decimals for getting user representation of a token amount.
      */
     function decimals() external pure returns (uint8);
 
@@ -40,14 +40,14 @@ interface ITenderToken {
      * @notice The total supply of tender tokens in existence.
      * @dev Always equals to `_getTotalPooledTokens()` since token amount
      * is pegged to the total amount of Tokens controlled by the protocol.
-     * @return total supply
+     * @return totalSupply total supply
      */
     function totalSupply() external view returns (uint256);
 
     /**
      * @notice Total amount of underlying tokens controlled by the Tenderizer.
      * @dev The sum of all Tokens balances in the protocol, equals to the total supply of TenderToken.
-     * @return total amount of pooled tokens
+     * @return totalPooledTokens total amount of pooled tokens
      */
     function getTotalPooledTokens() external view returns (uint256);
 
@@ -55,7 +55,7 @@ interface ITenderToken {
      * @notice The total amount of shares in existence.
      * @dev The sum of all accounts' shares can be an arbitrary number, therefore
      * it is necessary to store it in order to calculate each account's relative share.
-     * @return total amount of shares
+     * @return totalShares total amount of shares
      */
     function getTotalShares() external view returns (uint256);
 
@@ -64,13 +64,14 @@ interface ITenderToken {
      * @dev Balances are dynamic and equal the `_account`'s share in the amount of the
         total Tokens controlled by the protocol. See `sharesOf`.
      * @param _account address of the account to check the balance for
+     * @return balance token balance of `_account`
      */
     function balanceOf(address _account) external view returns (uint256);
 
     /**
      * @notice The amount of shares owned by an account
      * @param _account address of the account
-     * @return the amount of shares owned by `_account`.
+     * @return shares the amount of shares owned by `_account`.
      */
     function sharesOf(address _account) external view returns (uint256);
 
@@ -80,21 +81,21 @@ interface ITenderToken {
      * @dev This value changes when `approve` or `transferFrom` is called.
      * @param _owner address that approved the allowance
      * @param _spender address that is allowed to spend the allowance
-     * @return amount '_spender' is allowed to spend from '_owner'
+     * @return allowance amount '_spender' is allowed to spend from '_owner'
      */
     function allowance(address _owner, address _spender) external view returns (uint256);
 
     /**
      * @notice The amount of shares that corresponds to `_tokens` protocol-controlled Tokens.
      * @param _tokens amount of tokens to calculate shares for
-     * @return nominal amount of shares the tokens represent
+     * @return shares nominal amount of shares the tokens represent
      */
     function tokensToShares(uint256 _tokens) external view returns (uint256);
 
     /**
      * @notice The amount of tokens that corresponds to `_shares` token shares.
      * @param _shares the amount of shares to calculate the amount of tokens for
-     * @return the amount of tokens represented by the shares
+     * @return tokens the amount of tokens represented by the shares
      */
     function sharesToTokens(uint256 _shares) external view returns (uint256);
 
@@ -102,7 +103,7 @@ interface ITenderToken {
      * @notice Transfers `_amount` tokens from the caller's account to the `_recipient` account.
      * @param _recipient address of the recipient
      * @param _amount amount of tokens to transfer
-     * @return a boolean value indicating whether the operation succeeded.
+     * @return success a boolean value indicating whether the operation succeeded.
      * @dev Emits a `Transfer` event.
      * @dev Requirements:
      * - `_recipient` cannot be the zero address.
@@ -115,7 +116,7 @@ interface ITenderToken {
      * @notice Sets `_amount` as the allowance of `_spender` over the caller's tokens.
      * @param _spender address of the spender allowed to approve tokens from caller
      * @param _amount amount of tokens to allow '_spender' to spend
-     * @return a boolean value indicating whether the operation succeeded.
+     * @return success a boolean value indicating whether the operation succeeded.
      * @dev Emits an `Approval` event.
      * @dev Requirements:
      * - `_spender` cannot be the zero address.
@@ -128,7 +129,7 @@ interface ITenderToken {
      * allowance mechanism. `_amount` is then deducted from the caller's allowance.
      * @param _sender address of the account to transfer tokens from
      * @param _recipient address of the recipient
-     * @return a boolean value indicating whether the operation succeeded.
+     * @return success a boolean value indicating whether the operation succeeded.
      * @dev Emits a `Transfer` event.
      * @dev Emits an `Approval` event indicating the updated allowance.
      * @dev Requirements:
@@ -147,6 +148,7 @@ interface ITenderToken {
      * @notice Atomically increases the allowance granted to `_spender` by the caller by `_addedValue`.
      * @param _spender address of the spender allowed to approve tokens from caller
      * @param _addedValue amount to add to allowance
+     * @return success a boolean value indicating whether the operation succeeded.
      * @dev This is an alternative to `approve` that can be used as a mitigation for problems described in:
      * https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol#L42
      * @dev Emits an `Approval` event indicating the updated allowance.
@@ -159,6 +161,7 @@ interface ITenderToken {
      * @notice Atomically decreases the allowance granted to `_spender` by the caller by `_subtractedValue`.
      * @param _spender address of the spender allowed to approve tokens from caller
      * @param _subtractedValue amount to subtract from current allowance
+     * @return success a boolean value indicating whether the operation succeeded.
      * @dev This is an alternative to `approve` that can be used as a mitigation for problems described in:
      * https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol#L42
      * @dev Emits an `Approval` event indicating the updated allowance.
@@ -172,7 +175,7 @@ interface ITenderToken {
      * @notice Mints '_amount' of tokens for '_recipient'
      * @param _recipient address to mint tokens for
      * @param _amount amount to mint
-     * @return a boolean value indicating whether the operation succeeded.
+     * @return success a boolean value indicating whether the operation succeeded.
      * @dev Only callable by contract owner
      * @dev Calculates the amount of shares to create based on the specified '_amount'
      * and creates new shares rather than minting actual tokens
@@ -185,7 +188,7 @@ interface ITenderToken {
      * @notice Burns '_amount' of tokens from '_recipient'
      * @param _account address to burn the tokens from
      * @param _amount amount to burn
-     * @return a boolean value indicating whether the operation succeeded.
+     * @return success a boolean value indicating whether the operation succeeded.
      * @dev Only callable by contract owner
      * @dev Calculates the amount of shares to destroy based on the specified '_amount'
      * and destroy shares rather than burning tokens
