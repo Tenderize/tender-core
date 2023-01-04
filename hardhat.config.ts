4f2c8772e0348a05cd16c5611aa66cd991767b67
kyriediculous <vergauwennico@gmail.com>
2023-01-04 20:08:53 +0100

    enable optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -46,6 +46,7 @@ const config: HardhatUserConfig = {
         version: '0.8.4',
         settings: {
           optimizer: {
+            enabled: true,
             runs: 200
           }
         }
